347b7c0233d16e5994dba4fe23227129e98c1fff
kotopyos <115741086+kotopyos@users.noreply.github.com>
2023-07-29 15:33:41 +0300

    Changing remote server again...

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,7 +44,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String videoUrl = "http://95.216.214.178:8080/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,8 +22,8 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "108.0";
-//       Configuration.remote = "http://95.216.214.178:8080/wd/hub";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+       Configuration.remote = "http://95.216.214.178:8080/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
