a53e1f4ebab05aa69191ba072bf4a388c96a0e47
kotopyos <115741086+kotopyos@users.noreply.github.com>
2023-07-29 15:43:24 +0300

    Parametrization Jenkins job

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,11 +19,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "108.0";
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "108.0");
        Configuration.remote = "http://95.216.214.178:8080/wd/hub";
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; // not available from jenkins
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
