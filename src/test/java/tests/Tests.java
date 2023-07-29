5f90f7e86adcfe409daf7da82a6c8ef327232da7
kotopyos <115741086+kotopyos@users.noreply.github.com>
2023-07-29 15:31:34 +0300

    Changing remote server again...

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.qameta.allure.*;
+import jdk.jfr.Category;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
